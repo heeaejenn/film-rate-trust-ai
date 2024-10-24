1ce35abbc1d7145e06ca8915bf099cb09b06e72f
heeaejenn <heeaejenn@gmail.com>
2024-10-24 20:35:22 +0900

    홈: 랜딩 페이지
    - 포스터 이미지 삽입
    - 영화 텍스트에 각 영화 리뷰 페이지로 연결되는 링크 삽입

---
+++ pages/home.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+st.set_page_config(page_title="FilmRateTrust AI", page_icon="data/ai_icon.webp", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+st.title('FilmRateTrust AI')
+st.subheader('영화 관람평 평가 AI')
+
+st.markdown('<span style="font-size: 18px;">위 5가지 영화에 대한 네이버 관람평 요약본을 확인해보세요!</span>', unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image("data/movie_poster/notebook.jpeg", width=300, use_column_width=True)
+    st.page_link("pages/notebook.py", label="[1위] 노트북 (2004)")
+
+with col2:
+    st.image("data/movie_poster/konan.jpeg", width=300, use_column_width=True)
+    st.page_link("pages/konan.py", label="[2위] 명탐정 코난 (2024)")
+
+with col3:
+    st.image("data/movie_poster/veteran.jpeg", width=300, use_column_width=True)
+    st.page_link("pages/veteran.py", label="[3위] 베테랑2 (2024)")
+
+col4, col5, col6 = st.columns(3)
+
+with col4:
+    st.image("data/movie_poster/joker.jpeg", width=300, use_column_width=True)
+    st.page_link("pages/joker.py", label="[4위] 조커: 폴리 아 되 (2024)")
+
+with col5:
+    st.image("data/movie_poster/family.jpeg", width=300, use_column_width=True)
+    st.page_link("pages/family.py", label="[5위] 가문의 영광: 리턴즈 (2023)")
+
+with col6:
+    st.image("data/movie_poster/blank_space.png", width=300, use_column_width=True)
+
