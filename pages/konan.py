a7e5a346da9340dba792fb85e96961da947e7147
heeaejenn <heeaejenn@gmail.com>
2024-10-25 12:04:18 +0900

    - 탭에 ChatGPT로 생성한 icon 추가 - 평점 별 요약글 조회하기 selectbox에 나오는 text는 mysql 데이터베이스랑 연동 (임의로 본인이 로컬에서 만든 임시 테이블과 연결)

--- pages/konan.py
+++ pages/konan.py
@@ -1,11 +1,22 @@
 import streamlit as st
 from test_codes.selectbox import *
 
-st.set_page_config(page_title="FilmRateTrust AI", page_icon="data/ai_icon.webp", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 st.title('명탐정 코난: 시한장치의 마천루 (2024)')
 st.markdown('<span style="font-size: 18px;">네이버 리뷰 평점 별 관람평 요약</span>', unsafe_allow_html=True)
 
 st.image("data/movie_poster/konan.jpeg", width=200, use_column_width=False)
 
-ranking_selectbox()
+selected_option = ranking_selectbox()
+
+if st.button("조회하기"):
+    if selected_option == "⭐⭐⭐⭐⭐":
+        st.write(df['summary'][0])
+    elif selected_option == "⭐⭐⭐⭐":
+        st.write(df['summary'][1])
+    elif selected_option == "⭐⭐⭐":
+        st.write(df['summary'][2])    
+    elif selected_option == "⭐⭐":
+        st.write(df['summary'][3])  
+    else:
+        st.write(df['summary'][4])
