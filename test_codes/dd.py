6bb24954a221b8ad9deb0737d57e25570e2466a8
heeaejenn <heeaejenn@gmail.com>
2024-10-29 18:44:32 +0900

    Delete test_codes directory
    불필요한 코드 삭제. 필요한 파일은 추후 재업로드 예정

--- test_codes/dd.py
+++
@@ -1,6 +0,0 @@
-### SQL DB 연결
-
-# Initialize connection.
-# conn = st.connection('mysql', type='sql')
-# # Perform query.
-# summarized_reviews_db = conn.query('SELECT * from summarized_reviews;', ttl=600)
